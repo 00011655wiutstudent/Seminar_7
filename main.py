79ad34eb0f793cbb4011c7e2a23a4df51143757d
00011655wiutstudent <00011655wiutstudent@gmail.com>
2022-12-10 11:04:11 +0500

    Classroom tasks are done

--- main.py
+++ main.py
@@ -19,3 +19,51 @@ for i in mark_values:
 
 marks_sum = marks_sum/j
 print(marks_sum)
+
+def histogram(s):
+    d = dict()
+    for c in s:
+        if c not in d:
+            d[c] = 1
+        else:
+            d[c] += 1
+    return d
+
+h = histogram('brontosaurus')
+print(h)
+
+#Looping
+def print_hist(h):
+    for c in h:
+        print(c, h[c])
+
+h = histogram('parrot')
+print_hist(h)
+
+#Reverse look up
+def reverse_lookup(d, v):
+    for k in d:
+        if d[k] == v:
+            return k
+    raise LookupError()
+
+h = histogram('parrot')
+key = reverse_lookup(h, 1)
+print(key)
+
+#Task  8
+#Get function of dictionary
+csf = {
+'cw1-weight': 0.4,
+'cw1-mark':79,
+'exam-weight':0.6,
+'exam-mark':65
+}
+weight_1 = csf.get('cw1-weight', 0)
+weight_2 = csf.get('exam-weight', 0)
+mark_1 = csf.get('cw1-mark', 0)
+mark_2 = csf.get('exam-mark', 0)
+
+marks_csf = weight_1*mark_1+weight_2*mark_2
+print(marks_csf)
+
